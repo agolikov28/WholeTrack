0815143fe3ad2dfccd78f5ab506d52953804bada
agolikov28 <agolikov@umd.edu>
2022-11-04 23:59:36 -0400

    finished main sliders

--- app/src/main/java/com/example/wholetrack/HomePageActivity.java
+++ app/src/main/java/com/example/wholetrack/HomePageActivity.java
@@ -2,10 +2,12 @@ package com.example.wholetrack;
 
 import android.content.Intent;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
@@ -30,13 +32,12 @@ public class HomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         }
 
-        chnageBarColour();
+        changeBarColour();
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
@@ -102,9 +103,10 @@ public class HomePageActivity extends AppCompatActivity {
 
     }
 
-    private void chnageBarColour() {
+    private void changeBarColour() {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
