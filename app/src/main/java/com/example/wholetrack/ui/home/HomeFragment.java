1eb1723458f5a9e37bc7a98236d2ca37065d1449
stephanezab <stephanezab@gmail.com>
2022-11-06 20:56:38 -0500

    scrollview

--- app/src/main/java/com/example/wholetrack/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/wholetrack/ui/home/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,6 +17,7 @@ import com.example.wholetrack.databinding.FragmentHomeBinding;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private Button b1, b2, b3, b4;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +26,39 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        b1 = binding.button0;
+        b2 = binding.button1;
+        b3 = binding.button2;
+        b4 = binding.button3;
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"Not yet implemented", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"Not yet implemented", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"Not yet implemented", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"Not yet implemented", Toast.LENGTH_LONG).show();
+            }
+        });
+
 
         //final TextView textView = binding.textHome;
         //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
